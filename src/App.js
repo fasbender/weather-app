ef8f8c743f209d049155a18ac3e6781596798e7f
fasbender <alamgirfaisalbin@gmail.com>
2021-07-15 22:47:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react"
+import axios from "axios"
 import './App.css';
 
 function App() {
+
+  const [weather, setWeather] = useState([])
+
+  const Weather = async() => {
+    try {
+      const temp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=dac242caca04bcb5e721534038fb57bd`)
+      setWeather(temp.data.main) 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    Weather()
+  }, [])
+  console.log(weather)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
